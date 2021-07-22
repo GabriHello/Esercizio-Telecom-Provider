1864ac874eb6e46ffd2f8e69515d1f86ad36287c
Gabriele Venuta <gabriele.venuta@gmail.com>
2021-07-22 22:13:36 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-Telecom-Provider/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Esercizio_Telecom_Provider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
