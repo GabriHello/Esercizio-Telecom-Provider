51066df25ef71998a60f8c92e2355955bd9f35cd
Gabriele Venuta <gabriele.venuta@gmail.com>
2021-07-22 22:18:12 +0200

    Programma inizializzato

--- Esercizio-Telecom-Provider/Program.cs
+++ Esercizio-Telecom-Provider/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Esercizio_Telecom_Provider
 {
---
+++ Esercizio-Telecom-Provider/Sim.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_Telecom_Provider
+{
+    class Sim
+    {
+
+        List<Telefonata> listaTelefonate = new List<Telefonata>();
+        public string numeroSim { get; }
+        public float creditoSim { get; }
+        public Sim(string numeroSim, float creditoSim, List<Telefonata> listaTelefonate)
+        {
+            this.numeroSim = numeroSim;
+            this.creditoSim = creditoSim;
+            this.listaTelefonate = listaTelefonate;
+        }
+
+
+
+
+
+
+
+
+    }
+}
